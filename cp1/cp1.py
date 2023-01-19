9103c17af4b9b1b120a77a338f1acae91c1b7294
aqnemo32 <s1973088@ed.ac.uk>
2023-01-19 14:04:27 +0000

    update

--- cp1/cp1.py
+++ cp1/cp1.py
@@ -41,31 +41,41 @@ im=plt.imshow(spin, animated=True)
 #update loop here - for Glauber dynamics
 
 for n in range(nstep):
+    # this goes through all of the points in the matrix
     for i in range(lx):
         for j in range(ly):
 
 #select spin randomly
             itrial=np.random.randint(0,lx)
             jtrial=np.random.randint(0,ly)
+            # this should flip one spin in the matrix, i think
             spin_new=-spin[itrial,jtrial]
 
+            # do the checking of the new spin and wether half or more the nearest neighbours are the same
+
+            delta_E = E_spin(spin_new, lx, ly) - E_spin(spin, lx, ly)
+
+            if delta_E <= 0: spin = spin_new
+
+            else:
+                p = np.exp(-delta_E/kT)
+                r = random.random()
+                if r <= p: spin = spin_new
+                else: spin = spin
+
+# check wether the new spin has at least half of the neighbours alike. 
+# if it does no need to calc ∂E as it will for sure be < 0 so new spin matrix can be adopted
+
+# if it does not then you do the metropolis test
 
 #compute delta E eg via function (account for periodic BC)
 # delta E is the difference in energy -> ∂E = E_new - E_original
-# if ∂E <0 then we keep the new conformation as it minimises the energy
+# if ∂E < =0 then we keep the new conformation as it minimises the energy
 # If ∂E>0 then the fip happens with a prob of e^(-∂E/k_b t)
 
 #perform metropolis test
 
-delta_E = E_spin(spin_new, lx, ly) - E_spin(spin, lx, ly)
 
-if delta_E <= 0: spin = spin_new
-
-else:
-    p = np.exp(-delta_E/kT)
-    r = random.random()
-    if r <= p: spin = spin_new
-    else: spin = spin
           
 #occasionally plot or update measurements, eg every 10 sweeps
     if(n%10==0): 
