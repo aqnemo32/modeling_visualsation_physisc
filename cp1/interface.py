90c3eecf7be0b7e1161f3ee92ba445005762a307
aqnemo32 <s1973088@ed.ac.uk>
2023-01-20 22:47:34 +0000

    interface update

--- cp1/interface.py
+++ cp1/interface.py
@@ -7,4 +7,4 @@ if choice == 'g' or choice == 'G':
     print("You have chose the Glauber Algorithm ")
     N = int(input("Input the size of the array (as an integer): "))
     T = float(input("Input the temperature of the system (as a float): "))
-    subprocess.run(f"python3 cp1_glauber.py {N} {T}")
+    subprocess.run(f"/Users/achillequarante/Documents/github/modeling_visualsation_physisc/cp1/python3 cp1_glauber.py {N} {T}")
