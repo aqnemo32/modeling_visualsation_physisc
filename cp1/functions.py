3d2cfa477d18142de70757c5409ffc10794d4a99
aqnemo32 <s1973088@ed.ac.uk>
2023-01-31 15:28:39 +0000

    update magnet .py

--- cp1/functions.py
+++ cp1/functions.py
@@ -97,7 +97,7 @@ def susceptibility(M, lx, kT):
     M_sq = np.square(M)
     M_sq_avg = np.average(M_sq)
 
-    return (1/((lx**2)*kT) * (M_sq_avg - np.square(M_avg)), np.std(M_sq) + 2 * np.std(M))
+    return (1/((lx**2)*kT) * (M_sq_avg - np.square(M_avg)), np.std(M_sq) + 2 * np.std(M), M_avg)
 
 
 def energy(spin, lx):
