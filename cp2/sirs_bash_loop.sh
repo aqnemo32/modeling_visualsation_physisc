ea8499e8e567740e0099192c300dfe83583dc8e9
aqnemo32 <s1973088@ed.ac.uk>
2023-02-25 21:52:31 +0100

    work sirs bash script

--- cp2/sirs_bash_loop.sh
+++ cp2/sirs_bash_loop.sh
@@ -2,10 +2,17 @@
 
 P2=$(echo " scale=2; 0.5" | bc)
 P3=$(echo "scale = 3; 0.0" | bc)
-for KVAL in {1..100}
-echo ${KVAL}
-# P1=$(echo " scale=3; 0.0 + $KVAL * 0.05" | bc)
-# echo $P1
+for DUM in {1..100}
+do
+    P3=$(echo " scale=3; $P3 + 0.05" | bc)
+    for KVAL in {1..100}
+    do
+        echo "$KVAL"
+        P1=$(echo " scale=3; 0.0 + ($KVAL-1) * 0.05" | bc)
+        echo "$P1"
+        python3 sirs.py 50 $P1 $P2 $P3 
 
-# python3 sirs.py 50 $P1 $P2 $P3
+        echo "$P3"
+    done
+
 
